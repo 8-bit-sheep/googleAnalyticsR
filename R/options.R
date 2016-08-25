27eb771dd4e75de47c2a7c6e4bf4490cbf648e07
Mark Edmondson <github@markedmondson.me>
2016-08-26 01:46:39 +0200

    auto-authentication

--- R/options.R
+++ R/options.R
@@ -1,20 +1,55 @@
 .onLoad <- function(libname, pkgname) {
   
-  op <- options()
-  op.googleAnalyticsR <- list(
-    googleAnalyticsR.client_id = "289759286325-97kjcr4m3f0089cfqcp9hbu9jl4b6bus.apps.googleusercontent.com",
-    googleAnalyticsR.client_secret = "U2F718SsJvb3fxAMrFQLM-oZ",
-    googleAnalyticsR.webapp.client_id = "289759286325-42j8nmkeq5n9v9eb1kiuj2i97v9oea1f.apps.googleusercontent.com",
-    googleAnalyticsR.webapp.client_secret = "0zBtmZ_klTEzXUaTUTP5AkNQ",
-    #     googleAnalyticsR.scope = c("https://www.googleapis.com/auth/analytics",
-    #                                "https://www.googleapis.com/auth/analytics.edit"),
-    googleAnalyticsR.scope = c("https://www.googleapis.com/auth/analytics"),
-    googleAnalyticsR.tryAttempts = 5,
-    googleAnalyticsR.raw_req = FALSE
-  )
-  toset <- !(names(op.googleAnalyticsR) %in% names(op))
-  if(any(toset)) options(op.googleAnalyticsR[toset])
+  invisible()
+  
+}
+
+.onAttach <- function(libname, pkgname){
+  
+  scopes <- getOption("googleAuthR.scopes.selected")
+  needed <- c("https://www.googleapis.com/auth/analytics", 
+              "https://www.googleapis.com/auth/analytics.readonly",
+              "https://www.googleapis.com/auth/analytics.manage.users.readonly",
+              "https://www.googleapis.com/auth/analytics.edit",
+              "https://www.googleapis.com/auth/analytics.manage.users",
+              "https://www.googleapis.com/auth/analytics.provision"	)
+
+  
+  if(!any(needed %in% scopes)){
+    ## no analytics scopes
+    packageStartupMessage("No analytics scopes found so setting scope to https://www.googleapis.com/auth/analytics.readonly")
+    packageStartupMessage("If you need other scopes set do so via options(googleAuthR.scopes.selected = c('scope1', 'scope2')) before loading library(googleAnalyticsR) and include one analytics scope.")
+    new_scopes <- "https://www.googleapis.com/auth/analytics.readonly"
+  } else {
+    ## analytics scopes set
+    new_scopes <- scopes
+  }
+  
+  options(googleAuthR.scopes.selected = new_scopes)
+  
+  if(Sys.getenv("GA_CLIENT_ID") != ""){
+    options(googleAuthR.client_id = Sys.getenv("GA_CLIENT_ID"))
+  }
+  
+  if(Sys.getenv("GA_CLIENT_SECRET") != ""){
+    options(googleAuthR.client_secret = Sys.getenv("GA_CLIENT_SECRET"))
+  }
+  
+  if(Sys.getenv("GA_WEB_CLIENT_ID") != ""){
+    options(googleAuthR.webapp.client_id = Sys.getenv("GA_WEB_CLIENT_ID"))
+  }
+  
+  if(Sys.getenv("GA_WEB_CLIENT_SECRET") != ""){
+    options(googleAuthR.webapp.client_id = Sys.getenv("GA_WEB_CLIENT_SECRET"))
+  }
+  
+  
+  if(Sys.getenv("GA_AUTH_FILE") != ""){
+    googleAnalyticsR::ga_auth()
+    packageStartupMessage("Successfully authenticated via ", Sys.getenv("GA_AUTH_FILE"))
+  }
   
   invisible()
   
 }
+
