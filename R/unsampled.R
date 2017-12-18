a10c144f6155599f1c2adb599ff8e661fb9c3e60
Jas Sohi <j450h1@gmail.com>
2017-12-17 20:52:08 -0800

    Create half of the unsampled download function

--- R/unsampled.R
+++ R/unsampled.R
@@ -57,3 +57,70 @@ ga_unsampled_list <- function(accountId,
   unsampled()
   
 }
+
+#' Download Unsampled Report
+#'
+#' @param accountId Account Id
+#' @param webPropertyId Web Property Id
+#' @param profileId Profile Id
+#' @param reportTitle Title of Unsampled Report (case-sensitive)
+#' @param download Default TRUE, whether to download or otherwise return as dataframe
+
+#' @return Unsampled Report Downloaded as CSV
+#' @importFrom httr GET
+#' @family managementAPI functions, googleDriveAPI functions
+#' @export
+
+ga_unsampled_download <- function(accountId,
+                              webPropertyId,
+                              profileId,
+                              reportTitle,
+                              download=TRUE){
+  
+  report <- ga_unsampled_list(accountId, webPropertyId, profileId) %>% 
+            .$items %>% 
+            map(unlist) %>% 
+            as_data_frame() %>% 
+            rename(documentId=driveDownloadDetails) %>% 
+            filter(title==reportTitle)
+  
+  if(nrow(report) == 0) {
+    cat("Report title not found. Please enter a valid title. Remember it is case-sensitive") 
+    stop()
+  }
+  
+  if(nrow(report) >= 1) {
+    cat("WARNING: There are multiple reports with the same title. Choosing the most recently created.") 
+    report <- report %>% filter(created==max(created))
+  }
+  
+  #now there is only 1 report
+  if(report$status != "COMPLETED") {
+    cat(sprintf("The unsampled report has not COMPLETED. It is currently %s. Please try again at a later time.", report$status)) 
+    stop()
+  }
+  if(report$downloadType != "GOOGLE_DRIVE") {
+    cat("The downloadType is not a Google Drive link and cannot be downloaded.") #Is there another valid type?
+    stop()
+  }
+  
+  #Get document metadata
+  #
+  url <- sprintf("https://www.googleapis.com/drive/v2/files/%s", report$documentId)
+  document <- gar_api_generator(url, 
+                                "GET")
+  document <- document() 
+  
+  unsampled <- gar_api_generator(url,
+                                 "GET",
+                                 path_args = list(
+                                   accounts = accountId,
+                                   webproperties = webPropertyId,
+                                   profiles = profileId,
+                                   unsampledReports = ""
+                                 ),
+                                 data_parse_function = function(x) x)
+  
+  unsampled()
+  
+}
