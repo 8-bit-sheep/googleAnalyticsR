fdd8eea5eb0cb3f7591f43897c3566d29f44fcb0
zselinger <zselinger@gmail.com>
2018-01-20 10:57:17 +0000

    Documentation update for new updated filter functions

--- R/filters.R
+++ R/filters.R
@@ -385,6 +385,59 @@ ga_filter_delete <- function(accountId, propertyId, viewId, filterId, removeFrom
 #' Create a new filter and add it to the view (optional).
 #'
 #' @param Filter The Filter object to be added to the account or view
+#' Refer to documentation here: https://developers.google.com/analytics/devguides/config/mgmt/v3/mgmtReference/#Filters
+#'
+#' ## Create a filter object for adding an IP exclusion:
+#' Filter <- list(
+#'                name = 'Exclude Internal Traffic',
+#'                type = 'EXCLUDE',
+#'                excludeDetails = list(
+#'                    field = 'GEO_IP_ADDRESS',
+#'                    matchType = 'EQUAL',
+#'                    expressionValue = '199.04.123.1',
+#'                    caseSensitive = 'False'
+#'                                     )
+#'               )
+#'
+#' ## Create a filter object for making campaign medium lowercase
+#' Filter <- list(
+#'                name = 'Lowercase Campaign Medium',
+#'                type = 'LOWERCASE',
+#'                lowercaseDetails = list(
+#'                    field = 'CAMPAIGN_MEDIUM'
+#'                                     )
+#'               )
+#'
+#' ## Create a filter object to append hostname to URI
+#' Filter <- list(
+#'                name = 'Append hostname to URI',
+#'                type = 'ADVANCED',
+#'                advancedDetails = list(
+#'                    fieldA = 'PAGE_HOSTNAME',
+#'                    extractA = '(.*)',
+#'                    fieldARequired = 'True',
+#'                    fieldB = 'PAGE_REQUEST_URI',
+#'                    extractB = '(.*)',
+#'                    fieldBRequired = 'False',
+#'                    outputConstructor = '$A1$B1',
+#'                    outputToField = 'PAGE_REQUEST_URI',
+#'                    caseSensitive = 'False',
+#'                    overrideOutputField = 'True'
+#'                                     )
+#'               )
+#'
+#' ## Create a filter object to add www hostname without it
+#' Filter <- list(
+#'                name = 'Search and Replace www',
+#'                type = 'SEARCH_AND_REPLACE',
+#'                searchAndReplaceDetails = list(
+#'                    field = 'PAGE_HOSTNAME',
+#'                    searchString = '^exampleUSA\\.com$',
+#'                    replaceString = 'www.exampleUSA.com',
+#'                    caseSensitive = 'False'
+#'                                     )
+#'               )
+#'
 #' @param accountId Account Id of the account to add the Filter to
 #' @param propertyId Property Id of the property to add the Filter to
 #' @param viewId View Id of the view to add the Filter to
@@ -426,7 +479,8 @@ ga_filter_add <- function(Filter, accountId, propertyId = FALSE, viewId = FALSE,
 
 #' Updates an existing filter.
 #'
-#' @param Filter The list to be turned to JSON to create a request body
+#' @param Filter The Filter object to be updated
+#' See examples from ga_filter_add()
 #' @param accountId Account Id of the account that contains the filter
 #' @param filterId The id of the filter to be modified
 #' @param method PUT by default. For patch semantics use PATCH
@@ -449,18 +503,18 @@ ga_filter_update <- function(Filter, accountId, filterId, method = "PUT") {
 }
 
 
-#' Create a new profile filter link.
+#' Apply an existing filter to view.
 #'
 #' @param filterId The id of the filter to be addedd to profile/view
 #' @param accountId Account Id of the account that contains the filter
-#' @param webPropertyId Web property Id to create profile filter link for
-#' @param profileId Profile/view Id to create profile filter link for
+#' @param propertyId Web property Id to create profile filter link for
+#' @param viewId Profile/view Id to create profile filter link for
 #'
 #' @importFrom googleAuthR gar_api_generator
 #' @family managementAPI functions
 #' @export
 
-ga_filter_apply_to_view <- function(filterId, accountId, webPropertyId, profileId) {
+ga_filter_apply_to_view <- function(filterId, accountId, propertyId, viewId) {
 
   body <- list(filterRef = list(id = filterId))
   url <- "https://www.googleapis.com/analytics/v3/management/"
@@ -468,8 +522,8 @@ ga_filter_apply_to_view <- function(filterId, accountId, webPropertyId, profileI
                                "POST",
                                path_args = list(
                                  accounts = accountId,
-                                 webproperties = webPropertyId,
-                                 profiles = profileId,
+                                 webproperties = propertyId,
+                                 profiles = viewId,
                                  profileFilterLinks = ""
                                ),
                                data_parse_function = function(x) x)
@@ -480,7 +534,11 @@ ga_filter_apply_to_view <- function(filterId, accountId, webPropertyId, profileI
 
 #' Update an existing profile filter link. Patch semantics supported
 #'
-#' @param profileFilterLink The list to be turned to JSON to create a request body
+#' @param viewFilterLink The viewFilterLink object
+#' ## Example:
+#' ## Changing the rank of the filter in the view:
+#' viewFilterLink <- list(rank = 4)
+#'
 #' @param accountId Account Id of the account that contains the filter
 #' @param propertyId Web property Id to which the profile filter link belongs
 #' @param viewId View Id to which the profile filter link belongs
@@ -490,7 +548,7 @@ ga_filter_apply_to_view <- function(filterId, accountId, webPropertyId, profileI
 #' @importFrom googleAuthR gar_api_generator
 #' @family managementAPI functions
 #' @export
-ga_filter_update_filter_link <- function(profileFilterLink, accountId, propertyId, viewId, linkId, method = "PUT") {
+ga_filter_update_filter_link <- function(viewFilterLink, accountId, propertyId, viewId, linkId, method = "PUT") {
 
     url <- "https://www.googleapis.com/analytics/v3/management/"
     f <- gar_api_generator(url,
@@ -503,5 +561,5 @@ ga_filter_update_filter_link <- function(profileFilterLink, accountId, propertyI
                                ),
                                data_parse_function = function(x) x)
 
-    f(the_body = profileFilterLink)
+    f(the_body = viewFilterLink)
 }
