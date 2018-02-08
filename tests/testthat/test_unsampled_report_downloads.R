cb3f03a26e99de586182ae37edbca5e5e3d6b939
Jas Sohi <j450h1@gmail.com>
2018-02-07 22:39:23 -0800

    Comment out test for now

--- tests/testthat/test_unsampled_report_downloads.R
+++ tests/testthat/test_unsampled_report_downloads.R
@@ -1,74 +1,75 @@
-options(googleAuthR.scopes.selected = c("https://www.googleapis.com/auth/analytics",
-                                        "https://www.googleapis.com/auth/drive")) #Need drive scope, not sure if this should be default for entire package
-
-getOptions(googleAuthR.scopes.selected)
-
-options(googleAuthR.client_id = Sys.getenv("GA_CLIENT_ID"))
-options(googleAuthR.client_secret = Sys.getenv("GA_CLIENT_SECRET"))
-library(googleAuthR)
-gar_auth(token = ".httr-oauth")
-
-library(dplyr)
-library(purrr)
-library(devtools)
-devtools::load_all()
-
-accountId = Sys.getenv("ACCOUNTID")
-webPropertyId = Sys.getenv("WEBPROPERTYID")
-profileId = Sys.getenv("PROFILEID")
-
-library(tidyverse)
-## download all unsampled reports and create a list of dataframes
-test <- ga_unsampled_list(accountId, webPropertyId, profileId) %>%
-  select(driveDownloadDetails, title) %>% 
-  na.omit() 
-
-#download
-walk2(unlist(test$driveDownloadDetails), 
-      unlist(test$title),
-      ga_unsampled_download)
-
-#list of dataframes
-dataframes_test <- map(unlist(test$driveDownloadDetails), 
-      ga_unsampled_download, downloadFile=FALSE)
-  
-## one small report  < 25 mb
-
-reportTitle <- "googleanalyticsR_test_download"
-
-#download - not working right now due to misuse of walk2 probably
-ga_unsampled_list(accountId, webPropertyId, profileId) %>%
-  filter(title == reportTitle) %>%
-  select(driveDownloadDetails, title) %>% 
-  na.omit() %>%
-  walk2(ga_unsampled_download)
-
-#works
-small <- ga_unsampled_list(accountId, webPropertyId, profileId) %>%
-  filter(title == reportTitle) %>%
-  select(driveDownloadDetails) %>% 
-  na.omit() %>%
-  walk(ga_unsampled_download)
-
-#doesn't work, but should in case user selects title too
-small <- ga_unsampled_list(accountId, webPropertyId, profileId) %>%
-  filter(title == reportTitle) %>%
-  select(driveDownloadDetails, title) %>% 
-  na.omit() %>%
-  map2(ga_unsampled_download)
-
-## one large report > 25 mb
-large_reportTitle <- "googleanalyticsR_test_download_over_25_mb"
-
-#download - not working right now due to misuse of walk2 probably
-ga_unsampled_list(accountId, webPropertyId, profileId) %>%
-  filter(title == large_reportTitle) %>%
-  select(driveDownloadDetails, title) %>% 
-  na.omit() %>%
-  walk2(ga_unsampled_download)
-
-large <- ga_unsampled_list(accountId, webPropertyId, profileId) %>%
-  filter(title == large_reportTitle) %>%
-  select(driveDownloadDetails, title) %>% 
-  na.omit() %>%
-  map2(ga_unsampled_download)
+# options(googleAuthR.scopes.selected = c("https://www.googleapis.com/auth/analytics",
+#                                         "https://www.googleapis.com/auth/drive")) #Need drive scope, not sure if this should be default for entire package
+# 
+# getOptions(googleAuthR.scopes.selected)
+# 
+# options(googleAuthR.client_id = Sys.getenv("GA_CLIENT_ID"))
+# options(googleAuthR.client_secret = Sys.getenv("GA_CLIENT_SECRET"))
+# library(googleAuthR)
+# gar_auth(token = ".httr-oauth")
+# 
+# library(dplyr)
+# library(purrr)
+# library(devtools)
+# library(tidyverse)
+# devtools::load_all()
+# 
+# accountId = Sys.getenv("ACCOUNTID")
+# webPropertyId = Sys.getenv("WEBPROPERTYID")
+# profileId = Sys.getenv("PROFILEID")
+# 
+# reportTitle <- "googleanalyticsR_test_download"
+# 
+# # Download 1 file
+# ga_unsampled_download(reportTitle,
+#                       accountId,
+#                       webPropertyId,
+#                       profileId)
+# # Download multiple reports with pipes
+# ga_unsampled_list(accountId, webPropertyId, profileId) %>%
+#   select(title) %>%
+#   unlist() %>%
+#   map(ga_unsampled_download, accountId, webPropertyId, profileId)
+# 
+# # Download multiple reports with lapply
+# ## download all unsampled reports
+# unsample_df <- ga_unsampled_list(accountId, webPropertyId, profileId)
+# lapply(unsample_df$title, ga_unsampled_download, accountId, webPropertyId, profileId)
+# 
+# # Save to one dataframe option
+# df <- ga_unsampled_download(
+#   reportTitle,
+#   accountId,
+#   webPropertyId,
+#   profileId,
+#   downloadFile = FALSE
+# )
+# 
+# # Save to list of dataframes using pipes
+# dfs <- ga_unsampled_list(accountId, webPropertyId, profileId) %>%
+#   select(title) %>%
+#   unlist() %>%
+#   map(ga_unsampled_download, accountId, webPropertyId, profileId, downloadFile = FALSE)
+# 
+# # Save to list of dataframes with lapply
+# 
+# unsample_df <- ga_unsampled_list(accountId, webPropertyId, profileId)
+# dfs <- lapply(unsample_df$title, ga_unsampled_download, accountId, webPropertyId, profileId, downloadFile = FALSE)
+# 
+# # Download 1 file larger than 25 mbs
+# reportTitle <- "googleanalyticsR_test_download_over_25_mb"
+# ga_unsampled_download(
+#   reportTitle,
+#   accountId,
+#   webPropertyId,
+#   profileId
+# )
+# 
+# # Save to dataframe
+# df <- ga_unsampled_download(
+#   reportTitle,
+#   accountId,
+#   webPropertyId,
+#   profileId,
+#   downloadFile = FALSE
+# )
