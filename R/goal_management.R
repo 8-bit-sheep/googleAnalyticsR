1252931a83a6a59c014239446291acc3cb9a7ab3
zselinger <zselinger@gmail.com>
2018-03-12 21:10:01 +0000

    Added preliminary ga_goal_add function

---
+++ R/goal_management.R
@@ -0,0 +1,61 @@
+#' Create a new goal.
+#'
+#' @param Goal The Goal object to be added to the view.  See examples.
+#' @param accountId Account Id of the account to add the Goal to
+#' @param webPropertyId Property Id of the property to add the Goal to
+#' @param viewId View Id of the view to add the Goal to
+#'
+#' @return The Goal object
+#'
+#' @seealso \url{https://developers.google.com/analytics/devguides/config/mgmt/v3/mgmtReference/#Goals}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' ## Create a Goal object for adding an IP exclusion:
+#' Goal <- list(
+#'                resource = list(
+#'                    id = '2',
+#'                    active = TRUE,
+#'                    name = 'Checkout',
+#'                    type = 'URL_DESTINATION',
+#'                    urlDestinationDetails = list(
+#'                         url = '\\/checkout\\/thank_you',
+#'                         matchType = 'Checkout Complete',
+#'                         caseSensitive = FALSE,
+#'                         firstStepRequired = FALSE,
+#'                         steps = list(
+#'                            number = c(1,2,3,4,5,6),
+#'                            name = c('Product','Cart','Contact','Shipping','Payment','Processing'),
+#'                            url = c('.*\\/products\\/.*','\\/cart','\\/checkout\\/contact_information','\\/checkout\\/shipping',
+#'                                    '\\/checkout\\/payment','\\/checkout\\/processing'),
+#' )
+#' )
+#'                                     )
+#'               )
+#'
+#'
+#' @importFrom googleAuthR gar_api_generator
+#' @import assertthat
+#' @family managementAPI functions
+#' @export
+ga_goal_add <- function(Goal,
+                        accountId,
+                        webPropertyId,
+                        viewId) {
+  
+  assert_that(is.list(Goal))
+  
+  url <- "https://www.googleapis.com/analytics/v3/management/"
+  f <- gar_api_generator(url,
+                         "POST",
+                         path_args = list(
+                           accounts = accountId,
+                           webproperties = webPropertyId,
+                           profiles = viewId,
+                           goals = ""
+                         ))
+  
+  out <- f(the_body = Goal)
+  
+}
