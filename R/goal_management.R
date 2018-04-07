0896918a4ef1256fd80f83f8457e1218b7e5c78f
Zoran Selinger <zselinger@gmail.com>
2018-04-08 00:08:54 +0100

    Adde ga_goal_update

--- R/goal_management.R
+++ R/goal_management.R
@@ -57,7 +57,7 @@
 #'       )
 #'     )
 #'   )
-#'  )
+#' )
 #'
 #' ## Create a Goal object based on an event:
 #' Goal <- list(
@@ -129,4 +129,69 @@ ga_goal_add <- function(Goal,
   
   out <- f(the_body = Goal)
   
+}
+
+
+#' Updates an existing goal.
+#'
+#' @param Goal The Goal object to be updated
+#' See examples from ga_goal_add()
+#' @param accountId Account Id of the account in which to modify the Goal
+#' @param webPropertyId Property Id of the property in which to modify the Goal
+#' @param viewId View Id of the view in which to modify the Goal
+#' @param goalId The id of the goal to be modified
+#' @param method PUT by default. For patch semantics use PATCH
+#' 
+#' @return A goalManagement object
+#' @seealso \url{https://developers.google.com/analytics/devguides/config/mgmt/v3/mgmtReference/#Goals} 
+#' @examples 
+#' 
+#' \dontrun{
+#' 
+#' # Change the goal 11 to visits over 3 minutes
+#' Goal <- list(
+#'   active = TRUE,
+#'   name = 'Stayed for more than 3 minutes',
+#'   type = 'VISIT_TIME_ON_SITE',
+#'   visitTimeOnSiteDetails = list(
+#'     comparisonType = 'GREATER_THAN',
+#'     comparisonValue = 180
+#'   )
+#' )
+#' ga_goal_update(Goal, accountId, propertyId, viewId, 11)
+#'
+#' # Change destination url for goal 17
+#' Goal <- list(
+#'     urlDestinationDetails = list(
+#'       url = '\\/checkout\\/success'
+#'     )
+#'   )
+#' 
+#' # Only the fields we're changing required because we're using PATCH method  
+#' ga_goal_update(Goal, accountId, propertyId, viewId, 17, method = "PATCH")
+#'   
+#' @importFrom googleAuthR gar_api_generator
+#' @family managementAPI functions
+#' @export
+ga_goal_update <- function(Goal, 
+                           accountId, 
+                           webPropertyId,
+                           viewId,
+                           goalId, 
+                           method = c("PUT","PATCH")){
+  
+  method <- match.arg(method)
+  
+  url <- "https://www.googleapis.com/analytics/v3/management/"
+  f <- gar_api_generator(url,
+                         method,
+                         path_args = list(
+                           accounts = accountId,
+                           webproperties = webPropertyId,
+                           profiles = viewId,
+                           goals = goalId
+                         ),
+                         data_parse_function = function(x) x)
+  
+  o <- f(the_body = Goal)
 }
