80e24c45bb1ef321b98dfe20d59e406129a68c57
Mark Edmondson <github@markedmondson.me>
2020-05-26 13:22:56 +0200

    check authentication a lot

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -11,7 +11,12 @@ options(googleAuthR.scopes.selected =
 # auth cache with custom project creds
 googleAuthR::gar_set_client()
 
-ga_auth(json_file = "/workspace/auth.json")
+if(file.exists("/workspace/auth.json")){
+  message("Auth on Cloud Build")
+  ga_auth(json_file = "/workspace/auth.json")
+  ga_account_list()$viewName
+}
+
 
 accountId <- 54019251
 webPropId <- "UA-54019251-4"
@@ -21,3 +26,11 @@ accountId2 <- 47480439
 webPropId2 <- "UA-47480439-2"
 ga_id2 <- 81416156
 
+test_that("Correct authentication", {
+  al <- ga_account_list()
+  expect_true(accountId %in% al$accountId)
+  expect_true(accountId2 %in% al$accountId)
+  expect_true(ga_id %in% al$viewId)
+  expect_true(ga_id2 %in% al$viewId)
+  
+})
